d16d1f6263fb49e0ca10c941c04ef16dcd69dced
geek-yang <liuyang.bach@gmail.com>
2024-01-07 22:25:52 +0100

    define tuples

---
+++ rust_by_example/2_primitives/2-2_tuples.rs
@@ -0,0 +1,26 @@
+// https://doc.rust-lang.org/rust-by-example/primitives/tuples.html
+
+fn main() {
+    // A tuple with a bunch of different types.
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+
+    // Values can be extracted from the tuple using tuple indexing.
+    println!("Long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+
+    let pair = (1, true);
+    println!("Pair is {:?}", pair);
+
+    // To create one element tuples, the comma is required to tell them apart
+    // from a literal surrounded by parentheses.
+    println!("One element tuple: {:?}", (5u32,));
+    println!("Just an integer: {:?}", (5u32));
+    
+    // Tuples can be destructured to create bindings.
+    let tuple = (1, "hello", 4.5, true);
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+}
